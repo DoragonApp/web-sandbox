396703fadedb48c7d75ea158147e5abffed88265
Wuzado <23276409+Wuzado@users.noreply.github.com>
2020-08-26 18:34:05 +0200

    Updates

---
+++ precache-manifest.1f9e0e65d10fd9ccb5da2e7bff06deba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a656ac85ba9e2fee90f548f82034d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b19a5e2cfadae219d69b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e95d3e21eeb8907f9bb",
+    "url": "/static/js/2.52fbf94b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.52fbf94b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b19a5e2cfadae219d69b",
+    "url": "/static/js/main.d4d8a612.chunk.js"
+  },
+  {
+    "revision": "515a55f5eeaed188d87b",
+    "url": "/static/js/runtime-main.f5fbfaff.js"
+  }
+]);
